4be69d73b52ad0b12fad514af1e4975e5cee1a98
lichuanyi <chuanyi.work@gmail.com>
2021-12-29 23:40:28 +0800

    统计下载总量方式改为根据开发者主页遍历方式

--- queryAllDownloads.py
+++ queryAllDownloads.py
@@ -1,4 +1,4 @@
-from queryDownloads import replace_readme_comments,get_downloads_from_url
+from queryDownloads import replace_readme_comments,get_downloads_from_url,get_developers_app_total_downloads
 import argparse
 
 if __name__ == "__main__":
@@ -6,16 +6,9 @@ if __name__ == "__main__":
 	# https://apps.garmin.cn/zh-CN/apps/dc6ceca8-6ec6-49f2-b711-4ebc0d347177
 	parser.add_argument("readme_file_name", help="readme_file_name")
 	parser.add_argument("badge_pattern", help="badge_pattern")
-	parser.add_argument('--ciq_id_list', nargs='+')
+	parser.add_argument('developer_id', help="developer id. e.g. cdc2c15c-3ac3-46c6-99ec-a9c0f4a135e0 from https://apps.garmin.com/en-US/developer/cdc2c15c-3ac3-46c6-99ec-a9c0f4a135e0/apps")
 
-	count = 0
 	options = parser.parse_args()
 	print(options)
-	for ciq_id in options.ciq_id_list:
-		if ciq_id is not None:
-			ciq_download_intl = get_downloads_from_url('https://apps.garmin.com/zh-CN/apps/' + ciq_id)
-			ciq_download_china = get_downloads_from_url('https://apps.garmin.cn/zh-CN/apps/' + ciq_id)
-			if ciq_download_intl and ciq_download_china:
-				downloads = int(ciq_download_intl) + int(ciq_download_china)
-				count += downloads
+	count = get_developers_app_total_downloads(options.developer_id)
 	replace_readme_comments(options.readme_file_name,options.badge_pattern,count)
--- queryDownloads.py
+++ queryDownloads.py
@@ -20,16 +20,41 @@ def replace_readme_comments(file_name,badge_pattern,downloads):
         f.truncate()
 
 def extract_downloads_from_html(html_text):
-    searchObj = regex.search(r'[^><]+(?<=\d+)(?=<\/span>)', html_text.decode('utf-8'))
+    searchObj = regex.search(r'[^><]+(?<=\d+)(?=<\/span>)', html_text)
     assert searchObj,'Nothing found!!'
     return searchObj[0]
 
+def extract_app_url_from_homepage(html_text):
+    searchObj = regex.findall(r'/en-US/apps/[0-9a-z\-]{0,36}', html_text)
+    assert searchObj,'Nothing found!!'
+    print('searchObj are:'.format(searchObj))
+    s = set()
+    for item in searchObj:
+        s.add(item)
+    return s
 
-def get_downloads_from_url(url):
+def get_developers_app_total_downloads(developer_id):
+    developer_home_page_url = 'https://apps.garmin.com/en-US/developer/{}/apps'.format(developer_id)
+    html_text = get_html_text_from_url(developer_home_page_url)
+    apps_uri_set = extract_app_url_from_homepage(html_text)
+    count = 0
+    for uri in apps_uri_set:
+        if uri is not None:
+            ciq_download_intl = get_downloads_from_url('https://apps.garmin.com' + uri)
+            ciq_download_china = get_downloads_from_url('https://apps.garmin.cn' + uri)
+            if ciq_download_intl and ciq_download_china:
+                downloads = int(ciq_download_intl) + int(ciq_download_china)
+                count += downloads
+    return count
+def get_html_text_from_url(url):
     print('requesting {}'.format(url))
     res = requests.get(url)
     html_text = res.content
     print('html text is \n {}'.format(html_text))
+    return html_text.decode('utf-8')
+
+def get_downloads_from_url(url):
+    html_text = get_html_text_from_url(url)
     return extract_downloads_from_html(html_text)
 
 def main(ciq_id,badge_pattern,readme_file_name):
